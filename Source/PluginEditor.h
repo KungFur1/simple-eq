f6bd995b51a176f389ceaf35e670afb5792066d7
Joris Plaščinskas <PlascinskasJoris@gmail.com>
2024-05-20 12:00:22 +0300

    fix: refactor peak coefficient generation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,7 +22,9 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class SimpleeqAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleeqAudioProcessorEditor  : public juce::AudioProcessorEditor,
+    juce::AudioProcessorParameter::Listener,
+    juce::Timer
 {
 public:
     SimpleeqAudioProcessorEditor (SimpleeqAudioProcessor&);
@@ -32,10 +34,30 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+    /** Indicates that a parameter change gesture has started.
+
+        E.g. if the user is dragging a slider, this would be called with gestureIsStarting
+        being true when they first press the mouse button, and it will be called again with
+        gestureIsStarting being false when they release it.
+
+        IMPORTANT NOTE: This will be called synchronously, and many audio processors will
+        call it during their audio callback. This means that not only has your handler code
+        got to be completely thread-safe, but it's also got to be VERY fast, and avoid
+        blocking. If you need to handle this event on your message thread, use this callback
+        to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
+        message thread.
+    */
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
+
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleeqAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider;
     CustomRotarySlider lowCutFreqSlider, lowCutSlopeSlider;
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -234,19 +234,24 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     return settings;
 }
 
-void SimpleeqAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings)
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
 {
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(
-        getSampleRate(),
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(
+        sampleRate,
         chainSettings.peakFreq,
         chainSettings.peakQuality,
         juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
+void SimpleeqAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings)
+{
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
 
     updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void SimpleeqAudioProcessor::updateCoefficients(Coefficients& old, const Coefficients& replacements)
+void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 {
     *old = *replacements;
 }
